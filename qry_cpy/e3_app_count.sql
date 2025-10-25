cce9ba47702c1be88e479731d6ab0b5a3608a9ef
JonghyeokPark <jonghyeok.park@hufs.ac.kr>
2025-10-26 01:59:24 +0900

    Query Fixed : Trimming Unnecessary Ones

--- qry_cpy/e3_app_count.sql
+++
@@ -1,8 +0,0 @@
-SELECT COUNT(*) 
-FROM files
-WHERE
-	((_size > 0 AND (media_type = '1')))
-	AND
-	((is_pending=0 AND is_trashed=0))
-ORDER BY (CASE WHEN datetaken > 0 THEN datetaken ELSE date_added*1000 END) DESC;
-;
